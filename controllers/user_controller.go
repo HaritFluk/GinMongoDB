42fae771a3e14a7621aa4791d3a5656bd38c980a
DekuKung <07ghostharitkumsan@gmail.com>
2022-03-14 18:42:29 +0700

    make CreateUser Function

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -16,7 +16,7 @@ import (
 )
 
 var userCollections *mongo.Collection = configs.GetCollection(configs.DB, "user")
-var validator = validator.New()
+var validate = validator.New()
 
 func CreateUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -25,14 +25,30 @@ func CreateUser() gin.HandlerFunc {
 		defer cancel()
 	
 		// validate the request body
-		if err := c.BindJSONRequest(&user); err != nil {
-			c.JSON(http.StatusBadRequest, responses.UserResponse(Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": err.Error()}))
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
 			return
 		}
 
 		// use the validator library to validate required fields
 		if validationErr := validate.Struct(&user); validationErr != nil {
-			
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": validationErr.Error()}})
+			return
 		}
+
+		newUser := models.User{
+			Id: 		primitive.NewObjectID(),
+			Name: 		user.Name,
+			Location: 	user.Location,
+			Title: 		user.Title,
+		}
+
+		result, err := userCollections.InsertOne(ctx, newUser)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+		
+		c.JSON(http.StatusCreated, responses.UserResponse{Status: http.StatusCreated, Message: "created", Data: map[string]interface{}{"data": result}})
 	}
 }
